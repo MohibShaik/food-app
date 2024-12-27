09a4b8a23713b82ec7c86df1f43ee3260a440dca
Mohib.ShaikMastan <Mohib.ShaikMastan@ivycomptech.com>
2024-12-27 19:49:36 +0530

    resolved api issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://food-app-server-a2ks.onrender.com/',
+  apiUrl: 'https://food-app-server-a2ks.onrender.com',
 };
