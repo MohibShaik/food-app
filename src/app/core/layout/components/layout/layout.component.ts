caeefb7e001b345bb0cdaeeeae1e752fd20de266
Mohib.ShaikMastan <Mohib.ShaikMastan@ivycomptech.com>
2025-02-02 23:24:11 +0530

    latest UI changes

--- src/app/core/layout/components/layout/layout.component.ts
+++ src/app/core/layout/components/layout/layout.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
-
+  links = [
+    { name: 'Home', route: '/home', disabled: false },
+    { name: 'Menu', route: '/menu', disabled: false },
+    { name: 'About', route: '/about', disabled: false },
+    { name: 'Contact', route: '/contact', disabled: false }
+  ];
 }
--- src/app/core/layout/layout-routing.module.ts
+++ src/app/core/layout/layout-routing.module.ts
@@ -5,23 +5,35 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LandingPageComponent,
-  },
-  {
-    path: 'menu',
-    component: MenuComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'menu',
+        component: MenuComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+    ],
   },
 ];
 
